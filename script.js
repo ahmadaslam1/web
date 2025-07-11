0fa357d2f12d2d38abf7fd43dcdd949a39515ddc
ahmadaslam1 <146559179+ahmadaslam1@users.noreply.github.com>
2025-07-11 16:15:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 // Scene setup
 const scene = new THREE.Scene();
+const loader = new THREE.TextureLoader();
+scene.background = loader.load("starry_night.jpg");
 const camera = new THREE.PerspectiveCamera(15, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
